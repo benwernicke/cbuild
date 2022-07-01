25406508deee90ac7f74a7a7987bdffba18c4960
benwernicke <ben.wernicke@student.hpi.uni-potsdam.de>
2022-07-01 09:08:55 +0200

    hello world!

---
+++ examples/hello_world/cbuild.c
@@ -0,0 +1,18 @@
+#define CBUILD
+#include "cbuild.h"
+
+int main(int argc, char** argv)
+{
+    auto_update();
+    compile_object("main.c", "-Ofast", "main.o");
+    compile_src("main", "main.o", "-Ofast");
+    if (argc > 1) {
+        if (strcmp(argv[1], "run") == 0) {
+            system("./main");
+        } else if (strcmp(argv[1], "clean") == 0) {
+            system("rm main.o");
+            system("rm main");
+        }
+    }
+    return 0;
+}
